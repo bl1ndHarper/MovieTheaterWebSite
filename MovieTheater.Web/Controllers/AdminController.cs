1504d2ce2c9004b9873e74a3273f8b4cba1eb837
Stanislav <nnoun352@gmail.com>
2025-06-25 09:52:08 +0300

    created view stub and controller for admin panel

---
+++ MovieTheater.Web/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using MovieTheater.Web.Models;
+using System.Diagnostics;
+
+namespace MovieTheater.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ILogger<AdminController> _logger;
+        public AdminController(ILogger<AdminController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Admin()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
