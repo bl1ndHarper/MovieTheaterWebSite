d80803e6626ce4d917786402d148a48a73083f4d
Yaroslav Postelnykov <yarikpost@gmail.com>
2025-07-03 18:42:09 +0300

    Home page date selection configured (#17)
    Movies and their sessions on the main page are now filtered by selected day;
    Details button in carousel configured to open the movie details for today;
    Minor MovieMainDto population bug fixed

--- MovieTheater.Web/ViewModels/HomePageViewModel.cs
+++ MovieTheater.Web/ViewModels/HomePageViewModel.cs
@@ -5,6 +5,7 @@ namespace MovieTheater.Web.ViewModels
     public class HomePageViewModel
     {
         public List<MovieMainDto> LatestMovies { get; set; } = new();
-        public List<MovieMainDto> AllSessions { get; set; } = new();
+        public List<MovieMainDto> MoviesByDay { get; set; } = new();
+        public string SelectedDate { get; set; } = string.Empty; // dd.MM
     }
 }
